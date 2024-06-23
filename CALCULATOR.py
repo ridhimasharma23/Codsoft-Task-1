6d64378b4d7d3b86f46afeca5a76963dfbd01c38
ridhimasharma23 <152064705+ridhimasharma23@users.noreply.github.com>
2024-06-23 01:10:36 -0400

    Add files via upload
    PYTHON CALCULATOR: To ease the calculations I have created a Pycalculator. The program begins with the display of logo and a welcome statement for increasing user experience. Then I have created functions for ADDITION, SUBTRACTION, MULTIPLICATION and DIVISION. The program asks the User to enter the digits and the type of operation to be performed .The final result will be displayed.

---
+++ CALCULATOR.py
@@ -0,0 +1,64 @@
+
+print('''
+ _____________________
+|  _________________  |
+| | PythonCalculator  0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------'  
+|_____________________|
+''')
+
+print("WELCOME TO MY CALCULATOR")
+print("LET'S BEGIN WITH THE OPERATION")
+def add(a,b):
+  return a+b
+
+def subtract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b):
+  return a / b
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  num1 = float(input("ENTER NUMBER:"))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("ENTER THE NEXT NUMBER:"))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit the program: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      print("THANK YOU FOR YOUR VALUABLE TIME")
+
+calculator()
+
+
+
+
+ 
